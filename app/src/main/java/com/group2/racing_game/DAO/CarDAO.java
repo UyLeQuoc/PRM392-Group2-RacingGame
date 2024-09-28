7014d684e4bb5e309b15f7490cf45d0e74e1db7d
hnam-truong <truonghahaonam@gmail.com>
2024-09-28 12:49:50 +0700

    race

--- app/src/main/java/com/group2/racing_game/DAO/CarDAO.java
+++ app/src/main/java/com/group2/racing_game/DAO/CarDAO.java
@@ -11,11 +11,11 @@ public class CarDAO {
     private static List<Car> carList = new ArrayList<>();
 
     private CarDAO() {
-        carList.add(new Car(1, "Xe lăn", R.drawable.seekbar_thumb, 5, 10, 5));
-        carList.add(new Car(2, "Xe đạp", R.drawable.seekbar_thumb, 0.5, 5.5, 4));
-        carList.add(new Car(3, "Mô tô", R.drawable.seekbar_thumb, -3, 9, 6));
-        carList.add(new Car(4, "Lamborghini", R.drawable.seekbar_thumb, 3, 3.5, 1));
-        carList.add(new Car(5, "F1", R.drawable.seekbar_thumb, -50, 50, 1));
+        carList.add(new Car(1, "Xe lăn", R.drawable.xe_lan, 5, 10, 5));
+        carList.add(new Car(2, "Xe đạp", R.drawable.xe_dap, 0.5, 5.5, 4));
+        carList.add(new Car(3, "Mô tô", R.drawable.mo_to, -3, 9, 6));
+        carList.add(new Car(4, "Lamborghini", R.drawable.lamborghini, 3, 3.5, 1));
+        carList.add(new Car(5, "F1", R.drawable.f1, 10, 50, 1.5));
     }
 
     public static CarDAO getInstance() {
